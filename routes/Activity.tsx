d31ae84c8dca7d529794d5d305dcb089b7c3c843
hyenaManager <zodiacjelly9@gmail.com>
2024-02-12 23:52:29 +0630

    remove unecessery imports

--- routes/Activity.tsx
+++ routes/Activity.tsx
@@ -1,10 +1,7 @@
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
-import { Ionicons } from "@expo/vector-icons";
 import { messageBoxStyle } from "../components/screens/inbox/messageBoxStyle";
-import SuggestionProfile from "../components/screens/inbox/suggestionCard";
 import { data } from "../dummyData";
-import NewFollowers from "../components/screens/newFollowerScreen/newFollower";
 import Suggestion from "../components/screens/inbox/suggestion";
 import Activity from "../components/screens/activityScreen/activities";
 
--- routes/detailScreen.tsx
+++ routes/detailScreen.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-// import HeaderNavigation from "../components/headerNavigation/headerNavigate";
 
 export function DetailScreen({ navigation }: any) {
   return (
